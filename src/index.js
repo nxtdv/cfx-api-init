60c9428c493f8ee47d9aaac9765d9cbf33f9a319
nxtdv <frags31560@gmail.com>
2022-07-21 11:43:00 +0200

    🐛 Rename function name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { players, server } from './modules/index.js'
 const cfxApi = async () => {
     const app = await cfx.fetchServer('j5x4lm') // Replace "j5x4lm" with your server ID
 
-    server.getServer(app) // Get server data
+    server.fetchServer(app) // Get server data
     players.getPlayerById(app, 6148) // Replace 6148 with a player ID
     players.getAllPlayers(app) // Get all players
     players.exportPlayersIntoJSON(app) // Export all players into a JSON file in the dist folder.
