c3d7a86159e8ac41760e8193cbbf5a347192647b
nxtdv <frags31560@gmail.com>
2022-07-22 18:40:04 +0200

    ✨ Add getServerVars

--- src/modules/serverData.js
+++ src/modules/serverData.js
@@ -2,4 +2,9 @@ export default {
     async fetchServer(serverData) {
         console.log(serverData)
     },
+
+    async getServerVars(serverData) {
+        const data = await serverData.data.vars
+        console.log(data)
+    },
 }
