bfc58381586183633998ad78ee92ee3efea836e1
nxtdv <frags31560@gmail.com>
2022-07-22 18:39:36 +0200

    🔨 Add getServerVars

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cfxApi = async () => {
     const app = await cfx.fetchServer('j5x4lm') // Replace "j5x4lm" with your server ID
 
     server.fetchServer(app) // Get server data
+    server.getServerVars(app) // Get server vars
     players.getPlayerById(app, 6148) // Replace 6148 with a player ID
     players.getAllPlayers(app) // Get all players
     players.exportPlayersIntoJSON(app) // Export all players into a JSON file in the dist folder.
